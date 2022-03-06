6a680f6d2451f86dbdfaca13217c9ce638266e5f
M Rifki Muchtar <thea.rm725@gmail.com>
2022-03-07 01:13:40 +0700

    Final sebelum deploy

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <b-nav id="nav" tabs>
+      <router-link to="/">Home</router-link>
+      <router-link to="/contact">Contact</router-link>
+      <router-link to="/project">Project</router-link>
+    </b-nav>
     <router-view />
   </div>
 </template>
@@ -24,9 +25,25 @@
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+    margin: 0.5rem;
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.btn-priv {
+  background-color: #42B983;
+  color: #fff;
+}
+.btn-priv:hover{
+  background-color: #26A76C;
+  color: #fff;
+}
+.jumbotron {
+    background-color: #CAE3F5;
+}
+body {
+  background-color: #F4FEFA;
 }
 </style>
---
+++ src/views/Project.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+
+    <div v-for="(project, index) in getProject" :key="index">
+      <div v-if="project.id == $route.params.id">
+        <div class="d-flex align-items-center">
+          <div class="flex-shrink-0" style="margin-right: 2rem">
+            <img :src="project.image" :alt="project.name">
+          </div>
+          <div class="flex-grow-1 ms-3">
+                <h5 class="card-title left">{{ project.name }}</h5>
+                <p class="card-text left">{{ project.description }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import json from "../assets/data.json";
+
+export default{
+  data() {
+    return {
+      getProject: json,
+    }
+  },
+}
+</script>
