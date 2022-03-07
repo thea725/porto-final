6b09698e77a8c0403d3b9e69bff7178b7bfe8779
M Rifki Muchtar <91528096+thea725@users.noreply.github.com>
2022-03-07 08:14:50 +0700

    Revert "deploy"

---
+++ src/views/Contact.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-container>
+    <div class="mb-3">
+      <label for="emailInput" class="form-label">Email address</label>
+      <input
+        type="email"
+        class="form-control"
+        id="emailInput"
+        placeholder="name@example.com"
+      />
+    </div>
+    <div class="mb-3">
+      <label for="subjectInput" class="form-label">Subject</label>
+      <input
+        type="email"
+        class="form-control"
+        id="subjectInput"
+        placeholder="Subject"
+      />
+    </div>
+    <div class="mb-3">
+      <label for="messageInput" class="form-label">Your Messages</label>
+      <textarea class="form-control" id="messageInput" rows="3"></textarea>
+    </div>
+    <div class="col-auto">
+      <button type="submit" class="btn btn-priv">Send</button>
+    </div>
+  </b-container>
+</template>
